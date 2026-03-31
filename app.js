766a31c2ea565e4997e2d8bc3d53859648337217
Caio Rocha <codebycaio@gmail.com>
2026-03-31 11:19:09 -0300

    feat: adiciona lógica de listas de apps e verificação por input
    - Cria arrays appsDeDano e appsDeCura com valores padrão.
    - Adiciona campo de input e botão de verificação no HTML.
    - Implementa lógica de comparação usando .includes() e .toLowerCase() no app.js.
    - Adiciona limpeza automática do campo de input após a verificação.

--- app.js
+++ app.js
@@ -26,3 +26,35 @@ btnCura.addEventListener('click', function() {
     rostoPet.innerText = "🤓";
     barraVida.innerText = vida;
 });
+
+const appsDeDano = ['tiktok', 'instagram', 'twitter', 'x', 'netflix'];
+const appsDeCura = ['vs code', 'visual studio code', 'notion', 'figma'];
+
+const inputApp = document.querySelector('#input-app');
+const btnVerificar = document.querySelector('#btn-verificar');
+
+btnVerificar.addEventListener('click', function() {
+    const appDigitado = inputApp.value.toLowerCase();
+
+    if (appsDeDano.includes(appDigitado)) {
+        vida = vida - 15;
+        rostoPet.innerText = "😭"
+        console.log("Procrastinou! Perdeu vida.");
+    } else if (appsDeCura.includes(appDigitado)) {
+        vida = vida + 10;
+         rostoPet.innerText = "🤓"
+         console.log("Focou! Ganhou vida.");
+    } else {
+        console.log("App neutro. Nada acontece.")
+    }
+
+    if (vida < 0) vida = 0;
+    if (vida > 100) vida = 100;
+
+    barraVida.innerText = vida;
+
+    inputApp.value = "";
+ });
+
+
+
